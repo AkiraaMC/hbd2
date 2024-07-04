cf9d2a36af177bf24d04d21fef80afbcbcac67bf
AkiraaMC <132826638+AkiraaMC@users.noreply.github.com>
2024-07-04 12:41:37 +0700

    init.js

--- builder/init.js
+++ builder/init.js
@@ -10,6 +10,30 @@ const {
 
 require("dotenv").config();
 
+## for Local Build
+
+# Mandatory Vars
+
+NAME='PIYUUU'
+## Name of receiver
+
+PIC='https://www.google.com/search?client=ms-android-xiaomi-terr1-rso2&sca_esv=5ab8962a38650a00&sxsrf=ADLYWIJYLWleQ4IGtaS3sbpp0hW0aEqBRg:1720064453148&q=foto+gemoy&udm=2&fbs=AEQNm0CvspUPonaF8UH5s_LBD3JPX4RSeMPt9v8oIaeGMh2T2D1DyqhnuPxLgMgOaYPYX7OtOF4SxbM4YPsyWUMdeXRPZhCDnq-5Z-yoSNSuzzuqe-AJEcgubqbZZMyt_grANnsjG430usNqxcfNkddhKrAc7ZdUqqRTe-LY6ueoAHENlT69FE4GZz5taUYsEQezNmmRFXnASP-Ran4mbpidC5JNYU_oow&sa=X&sqi=2&ved=2ahUKEwjLseG1u4yHAxWiwjgGHdLwBWsQtKgLegQIEhAB&biw=407&bih=798&dpr=3#vhid=oiZzF5lOwP-JgM&vssid=mosaic'
+## Name of image file
+
+# Optional Vars -- Do not add these vars to your env if you dont want to use them
+
+NICKNAME='PIYUUU'
+## A Nickname for more personalization
+
+HBD_MSG='HBD'
+## Custom HBD Greeting message
+
+SCROLL_MSG='https://telegra.ph/HAPPY-BIRTHDAY-07-04-40'
+## Name of the Scroll Message txt file
+
+OPEN_DATE='2024-08-14'
+## Active Date in YYYY-MM-DD format
+
 if (!process.env.NAME) throw new Error("Please specify NAME in environment.");
 if (!process.env.PIC) throw new Error("Please specify PIC in environment.");
 
